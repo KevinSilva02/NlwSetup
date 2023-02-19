dfcabf4623c7a40f1af7cbe5b1bf970d1cb5b154
Kevin Silva <91382242+KevinSilva02@users.noreply.github.com>
2023-02-19 10:30:56 -0300

    Initial commit

---
+++ mobile/src/screens/Habit.tsx
@@ -0,0 +1,128 @@
+import { Alert, ScrollView, Text, View } from "react-native";
+import { useRoute } from '@react-navigation/native';
+import dayjs from "dayjs";
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import { BackButton } from "../components/BackButton";
+import { ProgressBar } from "../components/ProgressBar";
+import { Checkbox } from "../components/Checkbox";
+import { useEffect, useState } from "react";
+import { Loadiang } from "../components/Loading";
+import { api } from "../lib/axios";
+import { HabitEmpty } from "../components/HabitEmpty";
+import clsx from "clsx";
+
+interface Params {
+    date: string
+}
+interface DayInfoProps {
+    completedHabits: string[]
+    posibleHabits: {
+        id: string;
+        title: string;
+    }[]
+
+}
+
+export function Habit(){
+    const route = useRoute();
+    const { date } = route.params as Params;
+    const [loading, setLoading] = useState(true);
+    const [dayInfo, setDayInfo] = useState <DayInfoProps | null>(null)
+    const [completedHabits, setCompletedHabits] = useState<string[]>([])
+
+    const parsedDate = dayjs(date);
+    const isDatePast = parsedDate.endOf('day').isBefore(new Date());
+    const dayOfWeek = parsedDate.format('dddd');
+    const dayAndMonth = parsedDate.format('DD/MM');
+
+    const habitsProgress = dayInfo?.posibleHabits.length ? generateProgressPercentage(dayInfo.posibleHabits.length, completedHabits.length) : 0
+
+    async function fetchHabits(){
+        try {
+            setLoading(true)
+
+            const response = await api.get('/day', { params: {date}});
+            setDayInfo(response.data);
+
+            setCompletedHabits(response.data.completedHabits)
+            
+        } catch (error) {
+            console.log(error)
+            Alert.alert('Ops', 'Não foi possivel carregar')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    async function handleToggleHabit(habitId: string){
+        try {
+            await api.patch(`/habits/${habitId}/toggle`);
+
+            if(completedHabits.includes(habitId)){
+                setCompletedHabits(prevState => prevState.filter(habit => habit !== habitId));
+            } else {
+                setCompletedHabits(prevState => [...prevState, habitId]);
+            }
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Ops','Não foi possivel atualizar os status do hábito');
+        }
+    }
+
+    useEffect(() => {
+        fetchHabits();
+    },[])
+
+    if(loading){
+        return(
+            <Loadiang />
+        )
+    }
+
+    return(
+        <View className="flex-1 bg-background px-8 pt-16" >
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{paddingBottom: 100}}
+            >
+                <BackButton />
+
+                <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+                    {dayOfWeek}
+                </Text>
+                
+                <Text className="mt-6 text-white font-extrabold text-3xl">
+                    {dayAndMonth}
+                </Text>
+
+                <ProgressBar progress={habitsProgress} />
+
+                <View className={clsx("mt-6",{
+                    ["opacity-50"]: isDatePast
+                })}>
+                    {
+                        dayInfo?.posibleHabits ? 
+                        dayInfo?.posibleHabits.map(habit => (
+                            <Checkbox 
+                                key={habit.id}
+                                title={habit.title}
+                                disabled={isDatePast}
+                                checked={completedHabits.includes(habit.id)}
+                                onPress={()=> handleToggleHabit(habit.id)}
+                            />
+                        )):
+                        <HabitEmpty />
+                    }
+
+                </View>
+                {
+                    isDatePast && (
+                        <Text className="text-white mt-10 text-center">
+                            Você não pode editar hábitos de uma data passada 
+                        </Text>
+                    )
+                }
+            </ScrollView>
+        </View>
+    )
+}
---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { Alert, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+import { Feather } from '@expo/vector-icons';
+import colors from "tailwindcss/colors";
+import { api } from "../lib/axios";
+
+const avaiableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+
+export function New(){
+    const git = 'hub';
+    const [weekDays, setWeekDays] = useState<number[]>([])
+    const [title, setTitle] = useState('');
+
+    function handleToggleWeekDay(weekDayIndex: number){
+        if(weekDays.includes(weekDayIndex)){
+            setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+        } else {
+            setWeekDays(prevState => [...prevState, weekDayIndex]);
+        }
+    }
+
+    async function handleCreateNewHabit(){
+        try{
+            if(!title.trim() || weekDays.length === 0){
+                return Alert.alert('Novos', 'Informe um novo hábito e escolha um dia')
+            }
+
+            await api.post('/habit', { title, weekDays })
+
+            setTitle('')
+            setWeekDays([])
+
+
+            Alert.alert('Novos', 'Habito criado com sucesso')
+
+        } catch(erro) {
+            console.log(erro)
+        }
+
+    }
+    return(
+        <View className="flex-1 bg-background px-8 pt-16" >
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 100}} >
+                <BackButton /> 
+
+                <Text className="mt-6 text-white font-extrabold text-3xl" >
+                    Criar hábito
+                </Text>
+                
+                <Text className="mt-6 text-white font-semibold text-base " >
+                    Qual o seu comprometimento?
+                </Text>
+
+                <TextInput 
+                    className="h-12 p-4 rounded-lg mt-3 bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+                    placeholder="Exercícios, dormir bem, etc..."
+                    placeholderTextColor={colors.zinc[400]}
+                    onChangeText={setTitle}
+                    value={title}
+                />
+
+                <Text className="font-semibold mt-4 mb-3 text-white text-base">
+                    Qual a recorrência?
+                </Text>
+
+                {
+                    avaiableWeekDays.map((weekDay, i) => (
+                        <Checkbox 
+                            key={weekDay} 
+                            title={weekDay} 
+                            onPress={() => handleToggleWeekDay(i)} 
+                            checked={weekDays.includes(i)}
+                        />
+                    )) 
+                }
+
+                <TouchableOpacity activeOpacity={0.7} className=" w-full h-14 items-center justify-center bg-green-600 rounded-md mt-6 flex-row" onPress={handleCreateNewHabit} >
+                    <Feather name="check" size={20} color={colors.white} />
+
+                    <Text className="font-semibold text-base text-white ml-2">
+                        Confirmar
+                    </Text>
+                </TouchableOpacity>
+
+            </ScrollView>
+        </View>
+    )
+}
